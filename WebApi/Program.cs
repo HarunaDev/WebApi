7d1adfc87de31cff78bac51fe93ee8dc680dff0b
Alvin Haruna <harunaalvin69@gmail.com>
2026-05-30 14:02:46 +0100

    created services to handle dependency injection

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -7,7 +7,31 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 // builder.Services.AddHttpLogging((o) => { });
 
+// builder.Services.AddSingleton<IMyService, MyService>();
+// builder.Services.AddScoped<IMyService, MyService>();
+builder.Services.AddTransient<IMyService, MyService>();
+
 var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+    var myService = context.RequestServices.GetRequiredService<IMyService>();
+    myService.LogCreation("First Middleware");
+    await next.Invoke();
+});
+app.Use(async (context, next) =>
+{
+    var myService = context.RequestServices.GetRequiredService<IMyService>();
+    myService.LogCreation("Second Middleware");
+    await next.Invoke();
+});
+
+app.MapGet("/", (IMyService myService) =>
+{
+    myService.LogCreation("Root");
+
+    return Results.Ok("Check the console for service creation logs");
+});
 
 if (app.Environment.IsDevelopment())
 {
@@ -19,6 +43,26 @@ if (app.Environment.IsDevelopment())
 app.MapControllers();
 
 app.Run ();
+
+public interface IMyService
+{
+    void LogCreation(string message);
+}
+
+public class MyService : IMyService
+{
+    private readonly int _serviceId;
+
+    public MyService()
+    {
+        _serviceId = new Random().Next(100000, 999999);
+    }
+
+    public void LogCreation(string message)
+    {
+        Console.WriteLine($"{message} - Service ID: {_serviceId}");
+    }
+}
 
 // app.UseHttpLogging();
 
